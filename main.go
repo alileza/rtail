1c9a2cf78c3b98c6c287c13bb690d2360d678b1f
Ali Reza <alirezayahya@gmail.com>
2017-01-04 15:54:17 +0700

    add config support

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
@@ -15,27 +17,56 @@ func main() {
 	os.Exit(Main())
 }
 
-var colorList = []func(string, ...interface{}) string{
-	color.BlueString,
-	color.CyanString,
-	color.GreenString,
-	color.MagentaString,
-	color.YellowString,
+var (
+	colorList = []func(string, ...interface{}) string{
+		color.BlueString,
+		color.CyanString,
+		color.GreenString,
+		color.MagentaString,
+		color.YellowString,
+	}
+	colorCounter int
+)
+
+type tconfig struct {
+	Name    string   `json:"name"`
+	Servers []string `json:"server_addresses"`
+	File    string   `json:"file"`
+	Options []string `json:"options"`
 }
-var colorCounter int
+
+var (
+	configFile = flag.String("config.file", "", "")
+
+	configs []tconfig
+)
 
 func Main() int {
-	var grCount int
+
+	if configFile := *configFile; configFile != "" {
+		b, err := ioutil.ReadFile(configFile)
+		if err != nil {
+			println(err.Error())
+			return 1
+		}
+		err = json.Unmarshal(b, &configs)
+		if err != nil {
+			println(err.Error())
+		}
+		return MainConfig()
+	}
+
 	if len(os.Args) < 2 {
 		fmt.Println("usage: rtail [<servers>] [-F | -f | -r] [-q] [-b # | -c # | -n #] <file>")
 		return 0
 	}
 	servers := os.Args[1]
 
+	var grCount int
 	errChan := make(chan error)
 
 	for _, server := range strings.Split(servers, ",") {
-		go run(server, flag.Args()[1:], errChan)
+		go run("", server, flag.Args()[1:], errChan)
 		grCount++
 	}
 	for grCount != 0 {
@@ -49,21 +80,42 @@ func Main() int {
 	return 0
 }
 
-func run(server string, command []string, err chan error) {
+func MainConfig() int {
+	var grCount int
+	errChan := make(chan error)
+	for _, cfg := range configs {
+		for _, server := range cfg.Servers {
+			go run(cfg.Name, server, append(cfg.Options, cfg.File), errChan)
+			grCount++
+		}
+
+	}
+	for grCount != 0 {
+		err := <-errChan
+		if err != nil {
+			println(err.Error())
+		}
+		grCount--
+	}
+
+	return 0
+}
+
+func run(name, server string, command []string, err chan error) {
 	cmds := []string{"ssh", server, "tail", strings.Join(command, " ")}
 	fmt.Println("Executing : ", cmds)
 
 	cmd := exec.Command(cmds[0], cmds[1:]...)
 
-	cmd.Stdout = &writer{prefix: randColor(fmt.Sprintf("[%s] ", server))}
-	cmd.Stderr = &writer{prefix: errColor(fmt.Sprintf("[%s] ERR : ", server))}
+	cmd.Stdout = &writer{prefix: randColor(fmt.Sprintf("[%s:%s] ", name, server))}
+	cmd.Stderr = &writer{prefix: errColor(fmt.Sprintf("[%s:%s] ERR : ", name, server))}
 
 	if errno := cmd.Run(); errno != nil {
-		err <- fmt.Errorf("[%s] %s", server, errno.Error())
+		err <- fmt.Errorf("[%s:%s] %s", name, server, errno.Error())
 		return
 	}
 
-	err <- fmt.Errorf("[%s] %s", server, "session closed")
+	err <- fmt.Errorf("[%s:%s] %s", name, server, "session closed")
 }
 
 func randColor(s string) string {
